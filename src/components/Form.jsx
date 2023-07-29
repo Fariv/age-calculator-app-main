9d1cb03b3196e126767aab8930d577e8d1f4c886
Ashraful Ferdous <ashraful@boo2.nl>
2023-07-29 16:20:16 +0600

    input handler functions added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,22 +1,36 @@
 import React, { Component } from 'react'
 import Input from './Input';
+import iconArrow from '../assets/images/icon-arrow.svg';
 
 export default class Form extends Component {
+    state = {
+        day: '',
+        month: '',
+        year: '',
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.getAttribute('name')]: e.target.value,
+        })
+    }
+
     render() {
+        const { day, month, year } = this.state;
         return (
             <form className="inputs-section">
                     
                 <div className="form-inputs-group">
 
-                    <Input label="Day" placeholder="DD" classtAtr="day" idAttr="day" />
-                    <Input label="Month" placeholder="MM" classtAtr="month" idAttr="month" />
-                    <Input label="Year" placeholder="YYYY" classtAtr="year" idAttr="year" />
+                    <Input label="Day" placeholder="DD" classtAtr="day" idAttr="day" value={day} onHandleChange={this.handleChange} />
+                    <Input label="Month" placeholder="MM" classtAtr="month" idAttr="month" value={month} onHandleChange={this.handleChange} />
+                    <Input label="Year" placeholder="YYYY" classtAtr="year" idAttr="year" value={year} onHandleChange={this.handleChange} />
                     
                 </div>
 
                 <div className="border">
                     <div className="hr"></div>
-                    <img src="./assets/images/icon-arrow.svg" alt="icon-arrow" className="calculate-button" />
+                    <img src={iconArrow} alt="icon-arrow" className="calculate-button" />
                 </div>
             </form>
         );
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,12 +2,19 @@ import React, { Component } from 'react'
 
 export default class Input extends Component {
     render() {
-        const { label, placeholder, classtAtr, idAttr } = this.props;
+        const { label, placeholder, classtAtr, idAttr, value, onHandleChange } = this.props;
         return (
             <div className="input-container day-input-container">
                 <label>{label}</label>
                 <div>
-                    <input className={classtAtr} id={idAttr} placeholder={placeholder} />
+                    <input 
+                        name={idAttr} 
+                        className={classtAtr} 
+                        id={idAttr} 
+                        placeholder={placeholder} 
+                        value={value} 
+                        onChange={onHandleChange} 
+                    />
                 </div>
             </div>
         )
